87050b4ef8e7d100a4d282d388f268d703edaeea
Lucas <lucas.barretto86@gmail.com>
2021-11-26 02:46:00 -0300

    Update chapter 6

--- chapter_6/dictionary.py
+++ chapter_6/dictionary.py
@@ -1,2 +1,45 @@
 # Dictionary
 # Kinda like Hash in Ruby or event kinda of struct
+
+alien_0 = {'color': 'green', 'points': 5 }
+
+print(alien_0['color'])
+print(alien_0['points'])
+print(alien_0)
+
+
+# Assign new keys and values
+
+alien_0['x'] = 25
+alien_0['y'] = 0
+
+print(f"Alien y = {alien_0['y']}, Alien x = {alien_0['x']}")
+
+
+# Removing dictionary value
+print(alien_0)
+
+del alien_0['color']
+
+print(alien_0)
+
+
+# Method get()
+print(alien_0.get('color', "Value not found"))
+
+
+# Method items()
+for key, value in alien_0.items():
+    print(key, value)
+
+
+# Method keys()
+print(alien_0.keys())
+
+
+# Method values()
+print(alien_0.values())
+
+
+# List inside a dictionary
+doll = {'height': 10, }
