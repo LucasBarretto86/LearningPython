f4296c155cbbc4155240c5cf23ab2f127e0ac1a9
Lucas <lucas.barretto86@gmail.com>
2021-12-01 01:00:01 -0300

    Update project

---
+++ chapter_2/types.py
@@ -0,0 +1,32 @@
+# Types in Python
+
+str_variable =  "Text"
+
+int_variable = 1
+
+float_variable = 1.2
+
+complex_variable = 2+3j
+
+list_variable = [1,2,3,4]
+
+tuple_variable = (1,2,3,4)
+
+range_variable = range(1, 5)
+
+dict_variable = {"name": "Lucas", "age": 35}
+
+bool_variable = False
+
+bytes_variable = 100101001
+
+
+print(type(str_variable))
+print(type(int_variable))
+print(type(float_variable))
+print(type(complex_variable))
+print(type(list_variable))
+print(type(range_variable))
+print(type(dict_variable))
+print(type(bool_variable))
+print(type(bytes_variable))
--- chapter_8/functions.py
+++ chapter_8/functions.py
@@ -60,3 +60,78 @@ print(set_user_name("Jorge", "Ben", "Jor"))
 print(set_user_name("Jorge", "Ben"))
 print(set_user_name("Jorge", "", "Jor"))
 
+
+# Function with list as paramenter
+def say_hellos(names):
+
+    for name in names:
+        print(f"Hello, {name}")
+
+
+say_hellos(["Lucas", "Fulano", "Sicrana"])
+
+# Function changing list data
+
+
+def modifying(list):
+    for item in list:
+        if type(item) == int or type(item) == float:
+            list[list.index(item)] = item**2
+    return list
+
+
+list = [1, 2, 3, 4]
+modifying(list)
+print(list)
+
+
+# Function transfering data between lists
+def transfer(invitees, confirmed):
+    while invitees:
+        guest = invitees.pop()
+        if len(guest) < 8:
+            confirmed.append(guest)
+    return confirmed
+
+
+invitees = ["Julio", "Mariano", "Adalberto", "Pachuca", "Guarapiranga"]
+
+confirmed = transfer(invitees, [])
+
+print(confirmed)
+
+
+# Function copy lists
+def titlelize(surnames):
+    for surname in surnames:
+        surnames[surnames.index(surname)] = surname.title()
+
+    return surnames
+
+# No copying
+surnames = ["silva", "olveira", "lopes", "souza"]
+print(titlelize(surnames))
+print(surnames)
+
+# Copying
+surnames = ["silva", "olveira", "lopes", "souza"]
+print(titlelize(surnames[:]))
+print(surnames)
+
+
+# Arbitrary paramenters
+def display_cars(*cars):
+    for car in cars:
+        print(car)
+
+display_cars("Ferrari", "Audi", "Ford", "Toyota", "Honda")
+
+
+# Positional and Abritary paramenters
+def display_fruits(limit, *fruits):
+    counter = 0
+    while counter < limit:
+        print(fruits[counter])
+        counter += 1
+
+display_fruits(3, "Banana", "Mango", "Apple", "Orange", "Grapes", "Lemon")
